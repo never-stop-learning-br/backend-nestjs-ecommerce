8caabdddf050fd7cf85b38280634e793e7cfc945
gabrielcdiniz <gabriel.dinizcd1997@gmail.com>
2022-10-28 15:38:49 -0300

    refactor: add prisma module and settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { Prisma, PrismaModule } from '@prisma-module/index';
-
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [PrismaModule],
   controllers: [AppController],
-  providers: [AppService, Prisma],
 })
 export class AppModule {}
--- src/prisma/prisma.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-
-import { Prisma } from '@prisma-module/index';
-
-@Module({
-  providers: [Prisma],
-})
-export class PrismaModule {}
